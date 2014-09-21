baa4ed8928cdc89acf7087fa1c10173ee55ed243
James Kyburz <james.kyburz@gmail.com>
2014-09-21 15:28:22 +0200

    Updated to latest pdfkit

--- index.js
+++ index.js
@@ -124,8 +124,7 @@ module.exports = function(opt) {
     return {
       layout: event.layout.type,
       margin: 0,
-      size: opt.size,
-      compress: false
+      size: opt.size
     };
   }
 
