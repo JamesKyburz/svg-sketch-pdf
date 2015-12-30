2a248b6929e1943d7d01ef10eb367f0d772b44bf
James Kyburz <james.kyburz@gmail.com>
2015-12-30 14:47:24 +0100

    standard

--- index.js
+++ index.js
@@ -1,167 +1,159 @@
-var through    = require('through');
-var Pdf        = require('pdfkit');
-var duplexer   = require('duplexer');
-var concat     = require('concat-stream');
-var rotate     = require('./rotate-event-counter-clockwise');
-var rgb2hex    = require('rgb2hex');
-var fs         = require('fs');
-var xtend      = require('xtend');
+var through = require('through')
+var Pdf = require('pdfkit')
+var duplexer = require('duplexer')
+var concat = require('concat-stream')
+var rotate = require('./rotate-event-counter-clockwise')
+var rgb2hex = require('rgb2hex')
+var xtend = require('xtend')
 
-module.exports = function(opt) {
-  var doc;
-  var readable = through();
-  var writable = through(create, done);
-  var s = duplexer(writable, readable);
+module.exports = function (opt) {
+  var doc
+  var readable = through.obj()
+  var writable = through.obj(create, done)
+  var s = duplexer(writable, readable)
 
-  var layout;
-  var restore = false;
-  var style;
-  var color;
+  var layout
+  var restore = false
+  var style
+  var color
 
-  function create(event) {
-    if (event.forEach) return event.forEach(create);
+  function create (event) {
+    if (event.forEach) return event.forEach(create)
     if (event.type === 'header') {
-      layout = event.layout.type;
-      createPage(event);
+      layout = event.layout.type
+      createPage(event)
     }
 
-    if (layout === 'portrait') rotate(event, 1, opt.size[1]);
+    if (layout === 'portrait') rotate(event, 1, opt.size[1])
 
     if (event.type === 'style') {
-      style = xtend(event.args);
+      style = xtend(event.args)
     }
 
     if (event.layout || event.type === 'style') {
-      restore = true;
-      doc.save();
+      restore = true
+      doc.save()
     }
     if (event.layout) {
       if (event.layout.font) {
-        doc.font(event.layout.font.name);
-        doc.fontSize(event.layout.font.size);
+        doc.font(event.layout.font.name)
+        doc.fontSize(event.layout.font.size)
       }
       if (event.layout.rotate) {
-        doc.rotate(event.layout.rotate.deg, {origin: [event.args.x, event.args.y]});
+        doc.rotate(event.layout.rotate.deg, {origin: [event.args.x, event.args.y]})
       }
     }
 
     if (event.args && event.args.stroke) {
-      color = event.args.stroke === 'transparent' ?
-        'transparent'
-        :
-        rgb2hex(event.args.stroke).hex
-      ;
+      color = event.args.stroke === 'transparent'
+        ? 'transparent'
+        : rgb2hex(event.args.stroke).hex
     }
 
-    if (style) applyStyles(style);
+    if (style) applyStyles(style)
 
-    if (color === 'transparent') return;
-
-    if (color) {
-      strokeColor(color);
+    if (color && color !== 'transparent') {
+      strokeColor(color)
     }
 
-    var f = doc[type(event)];
+    var f = doc[type(event)]
     if (f && args(event).length) {
-      f.apply(doc, args(event));
-      doc.stroke();
+      f.apply(doc, args(event))
+      doc.stroke()
       if (restore) {
-        doc.restore();
-        restore = false;
+        doc.restore()
+        restore = false
       }
     }
   }
 
-  function type(event) {
+  function type (event) {
     return event.type === 'rect' &&
-           event.args.rx ?
-           'roundedRect'
-           :
-          event.type
-    ;
+    event.args.rx
+      ? 'roundedRect'
+      : event.type
   }
 
-  function applyStyles(args) {
-    dash(args);
-    Object.keys(args).forEach(applyStyle.bind(null, args));
+  function applyStyles (args) {
+    dash(args)
+    Object.keys(args).forEach(applyStyle.bind(null, args))
   }
 
-  function applyStyle() {
-    var argv = [].slice.call(arguments);
-    var args = argv.shift();
-    var type = argv.shift();
+  function applyStyle () {
+    var argv = [].slice.call(arguments)
+    var args = argv.shift()
+    var type = argv.shift()
     if (doc[type]) {
-      doc[type](args[type]);
+      doc[type](args[type])
     }
   }
 
-  function strokeColor(color) {
-    doc.fillColor(color);
-    doc.strokeColor(color);
+  function strokeColor (color) {
+    doc.fillColor(color)
+    doc.strokeColor(color)
   }
 
-  function dash(style) {
-    var strokeDashArray = (style['stroke-dasharray'] || '').split(',');
+  function dash (style) {
+    var strokeDashArray = (style['stroke-dasharray'] || '').split(',')
     if (strokeDashArray.length === 2) {
-      doc.dash(+strokeDashArray[0], {space: +strokeDashArray[1]});
+      doc.dash(+strokeDashArray[0], {space: +strokeDashArray[1]})
     } else {
-      doc.undash();
+      doc.undash()
     }
   }
 
-  function args(event) {
-    return Object.keys(event.args).map(value).concat(style());
-    function value(key) {
+  function args (event) {
+    return Object.keys(event.args).map(value).concat(style())
+    function value (key) {
       if (event.type === 'text' && key === 'y') {
-        var yoffset = opt.yoffset ? opt.yoffset(event) : 0;
-        return event.args[key] - yoffset;
+        var yoffset = opt.yoffset ? opt.yoffset(event) : 0
+        return event.args[key] - yoffset
       } else {
-        return event.args[key];
+        return event.args[key]
       }
     }
 
-    function style() {
-      return (event.layout ?
-        event.layout.style
-        :
-        undefined
-      ) || [];
+    function style () {
+      return (event.layout
+        ? event.layout.style
+        : undefined
+      ) || []
     }
   }
 
-  function createPage(event) {
+  function createPage (event) {
     if (doc) {
-      doc.addPage(page(event));
-      return;
+      doc.addPage(page(event))
+      return
     } else {
-      doc = new Pdf(page(event));
+      doc = new Pdf(page(event))
     }
 
     if (opt.document) {
-      opt.document(doc);
+      opt.document(doc)
     }
   }
 
-  function page(event) {
+  function page (event) {
     return {
       layout: event.layout.type,
       margin: 0,
       compress: opt.compress,
       size: opt.size
-    };
-  }
-
-  function done() {
-    if (opt.base64) {
-      doc.pipe(concat(function(data) {
-        readable.emit('data', data.toString('base64'));
-        readable.end();
-      }));
-    } else {
-      doc.pipe(readable);
     }
-    doc.end();
   }
 
-  return s;
-};
+  function done () {
+    if (opt.base64) {
+      doc.pipe(concat(function (data) {
+        readable.emit('data', data.toString('base64'))
+        readable.end()
+      }))
+    } else {
+      doc.pipe(readable)
+    }
+    doc.end()
+  }
+
+  return s
+}
--- rotate-event-counter-clockwise.js
+++ rotate-event-counter-clockwise.js
@@ -1,85 +1,84 @@
-module.exports = rotate;
+module.exports = rotate
 
-var offset;
+var offset
 
-function rotate(event, scale, height) {
-  offset = height * (scale || 1);
+function rotate (event, scale, height) {
+  offset = height * (scale || 1)
   if (event.args) {
-    translateText(event);
-    translateEllipse(event);
-    swapXY(event);
-    translateDimensions(event);
-    translatePath(event);
+    translateText(event)
+    translateEllipse(event)
+    swapXY(event)
+    translateDimensions(event)
+    translatePath(event)
   }
 }
 
-function translateText(event) {
+function translateText (event) {
   if (event.type === 'text') {
-    event.layout = event.layout || {};
-    event.layout.rotate = event.layout.rotate || {};
-    event.layout.rotate.deg = event.layout.rotate.deg || 0;
-    event.layout.rotate.deg -= 90;
+    event.layout = event.layout || {}
+    event.layout.rotate = event.layout.rotate || {}
+    event.layout.rotate.deg = event.layout.rotate.deg || 0
+    event.layout.rotate.deg -= 90
   } else if (event.layout && event.layout.rotate) {
-    event.layout.rotate.deg -= 90;
+    event.layout.rotate.deg -= 90
   }
 }
 
-function swapXY(event) {
-  var tmp;
-  if ('undefined' !== typeof event.args.x) {
-    tmp = event.args.x;
-    event.args.x = event.args.y;
-    event.args.y = offset - tmp;
+function swapXY (event) {
+  var tmp
+  if (typeof event.args.x !== 'undefined') {
+    tmp = event.args.x
+    event.args.x = event.args.y
+    event.args.y = offset - tmp
   }
 }
 
-function translateEllipse(event) {
-  var tmp;
+function translateEllipse (event) {
+  var tmp
   if (event.type === 'ellipse') {
-    tmp = event.args.cx;
-    event.args.cx = event.args.cy;
-    event.args.cy = offset - tmp;
-    tmp = event.args.rx;
-    event.args.rx = event.args.ry;
-    event.args.ry = tmp;
+    tmp = event.args.cx
+    event.args.cx = event.args.cy
+    event.args.cy = offset - tmp
+    tmp = event.args.rx
+    event.args.rx = event.args.ry
+    event.args.ry = tmp
   }
 }
 
-function translateDimensions(event) {
-  var tmp;
+function translateDimensions (event) {
+  var tmp
   if (event.args.height) {
-    tmp = event.args.width;
-    event.args.width = event.args.height;
-    event.args.height = tmp;
-    event.args.y -= tmp;
+    tmp = event.args.width
+    event.args.width = event.args.height
+    event.args.height = tmp
+    event.args.y -= tmp
   }
 }
 
-function translatePath(event) {
-  var points;
+function translatePath (event) {
+  var points
   if (event.args.d) {
-      points = event.args.d.match(/\d+\.?\d*/g).map(Number);
-      var d =
-        'M ' +
-        points[1] +
+    points = event.args.d.match(/\d+\.?\d*/g).map(Number)
+    var d =
+    'M ' +
+      points[1] +
+      ',' +
+      (offset - points[0]) +
+      ' '
+
+    points.splice(0, 2)
+
+    var x, y
+
+    while ((x = points.shift()) !== undefined) {
+      y = points.shift()
+      d += '' +
+        'L ' +
+        y +
         ',' +
-        (offset - points[0]) +
-        ' '
-      ;
-      points.splice(0, 2);
+        (offset - x)
+    }
 
-      var x, y;
-
-      while((x = points.shift()) !== undefined) {
-        y = points.shift();
-        d += '' +
-          'L ' +
-          y +
-          ',' +
-          (offset - x)
-        ;
-      }
-
-      event.args.d = d;
+    event.args.d = d
   }
 }
